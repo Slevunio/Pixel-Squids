2353b2171dd70c96604796f756a44f0ad70302f0
Slevunio <slevunio@gmail.com>
2021-04-11 19:44:03 +0200

    laoding

--- frontend/src/app/pixel-squids/learning-page/learning-page.component.ts
+++ frontend/src/app/pixel-squids/learning-page/learning-page.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { IGuitarCord } from 'src/app/interfaces/IGuitarCord';
 import { GuitarCordsStoreService } from '../../store/guitar-cords-store/guitarCordsStoreService';
+const _ = require("lodash");
 @Component({
     selector: 'learning-page-component',
     templateUrl: './learning-page.component.html',
     styleUrls: ['./learning-page.component.scss']
 })
-export class LearningPageComponent implements OnInit {
+export class LearningPageComponent implements OnInit, AfterViewInit {
 
     public guitarCords: IGuitarCord[];
+    public isLoading: boolean;
 
-    constructor(private guitarCordsStoreService: GuitarCordsStoreService) { }
+    constructor(private guitarCordsStoreService: GuitarCordsStoreService, private cd: ChangeDetectorRef) { }
 
     public ngOnInit() {
+        this.isLoading = true;
         this.guitarCords = this.guitarCordsStoreService.getGuitarCords();
+    }
+
+    public ngAfterViewInit() {
+        
+        _.delay(() => {
+            this.isLoading = false;
+            this.cd.detectChanges();
+        }, 1000);
+        
     }
 }
