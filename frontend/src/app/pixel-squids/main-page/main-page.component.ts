433da9f62a4152d4cb233de2422b73334da86d8a
Slevunio <slevunio@gmail.com>
2021-04-26 21:01:01 +0200

    fix animations

--- frontend/src/app/pixel-squids/main-page/main-page.component.ts
+++ frontend/src/app/pixel-squids/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterService } from '../../services/RouterService';
 
 @Component({
     selector: 'main-page-component',
@@ -6,9 +7,21 @@ import { Component } from '@angular/core';
     styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
+
+    public wasStartPage!: boolean;
     public slides = [
         { path: '/assets/Slajd1.jpg' },
         { path: '/assets/Slajd2.jpg' },
         { path: '/assets/Slajd3.jpg' },
-        { path: '/assets/slajd4.jpg' }];
+        { path: '/assets/slajd4.jpg' }
+    ];
+
+    constructor(private routerService: RouterService) { }
+
+    public ngOnInit() {
+        this.routerService.wasPreviousStartPage$.subscribe(wasStartPage => {
+            this.wasStartPage = wasStartPage;
+        });
+    }
+
 }
